2235203657a9f60030950562bf334fd78afceb94
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-06-18 21:45:30 +0530

    Added functionality to the computer button

--- script.js
+++ script.js
@@ -100,75 +100,78 @@ function multiPlayerFunction() {
     }
 }
 
-function singlePlayerFunction() {
-    let gameBoard = initialGameBoard() ;
-    while(true){
-        [input,row,column] = getHumanInput() ;
-        if (gameBoard[row][column] === '') {
-            gameBoard[row][column] = input ;
-            console.log(gameBoard) ;
-        }
-        else{
-            prompt('Its already filled')
-            while(true){
-                [input,row,column] = getHumanInput() ;
-                if (gameBoard[row][column] === '') {
-                    gameBoard[row][column] = input ;
-                    console.log(gameBoard) ;
-                    break
-                }
-            }
-        }
+// function singlePlayerFunction() {
+//     let gameBoard = initialGameBoard() ;
+//     while(true){
+//         [input,row,column] = getHumanInput() ;
+//         if (gameBoard[row][column] === '') {
+//             gameBoard[row][column] = input ;
+//             console.log(gameBoard) ;
+//         }
+//         else{
+//             prompt('Its already filled')
+//             while(true){
+//                 [input,row,column] = getHumanInput() ;
+//                 if (gameBoard[row][column] === '') {
+//                     gameBoard[row][column] = input ;
+//                     console.log(gameBoard) ;
+//                     break
+//                 }
+//             }
+//         }
 
-        if(gameBoard[0][0] === "X" && gameBoard[0][1] === "X" && gameBoard[0][2] === "X" ||
-            gameBoard[1][0] === "X" && gameBoard[1][1] === "X" && gameBoard[1][2] === "X" ||
-            gameBoard[2][0] === "X" && gameBoard[2][1] === "X" && gameBoard[2][2] === "X" ||
-            gameBoard[0][0] === "X" && gameBoard[1][0] === "X" && gameBoard[2][0] === "X" ||
-            gameBoard[0][1] === "X" && gameBoard[1][1] === "X" && gameBoard[2][1] === "X" ||
-            gameBoard[0][2] === "X" && gameBoard[1][2] === "X" && gameBoard[2][2] === "X" ||
-            gameBoard[0][0] === "X" && gameBoard[1][1] === "X" && gameBoard[2][2] === "X" ||
-            gameBoard[0][2] === "X" && gameBoard[1][1] === "X" && gameBoard[2][0] === "X"
-        ){
-            console.log("Player1 Won") ;
-            break ;
-        }
+//         if(gameBoard[0][0] === "X" && gameBoard[0][1] === "X" && gameBoard[0][2] === "X" ||
+//             gameBoard[1][0] === "X" && gameBoard[1][1] === "X" && gameBoard[1][2] === "X" ||
+//             gameBoard[2][0] === "X" && gameBoard[2][1] === "X" && gameBoard[2][2] === "X" ||
+//             gameBoard[0][0] === "X" && gameBoard[1][0] === "X" && gameBoard[2][0] === "X" ||
+//             gameBoard[0][1] === "X" && gameBoard[1][1] === "X" && gameBoard[2][1] === "X" ||
+//             gameBoard[0][2] === "X" && gameBoard[1][2] === "X" && gameBoard[2][2] === "X" ||
+//             gameBoard[0][0] === "X" && gameBoard[1][1] === "X" && gameBoard[2][2] === "X" ||
+//             gameBoard[0][2] === "X" && gameBoard[1][1] === "X" && gameBoard[2][0] === "X"
+//         ){
+//             console.log("Player1 Won") ;
+//             break ;
+//         }
 
-        if(gameBoard[0][0] !== "" && gameBoard[0][1] !== "" && gameBoard[0][2] !== "" &&
-            gameBoard[1][0] !== "" && gameBoard[1][1] !== "" && gameBoard[1][2] !== "" &&
-            gameBoard[2][0] !== "" && gameBoard[2][1] !== "" && gameBoard[2][2] !== ""
-        ){
-            console.log('Draw')
-            break
-        }
+//         if(gameBoard[0][0] !== "" && gameBoard[0][1] !== "" && gameBoard[0][2] !== "" &&
+//             gameBoard[1][0] !== "" && gameBoard[1][1] !== "" && gameBoard[1][2] !== "" &&
+//             gameBoard[2][0] !== "" && gameBoard[2][1] !== "" && gameBoard[2][2] !== ""
+//         ){
+//             console.log('Draw')
+//             break
+//         }
     
-        [input,row,column] = getCompInput() ;
-        if (gameBoard[row][column] === '') {
-            gameBoard[row][column] = "O" ;
-            console.log(gameBoard) ;
-        }
-        else{
-            while(true){
-                [input,row,column] = getCompInput() ;
-                if (gameBoard[row][column] === '') {
-                    gameBoard[row][column] = "O" ;
-                    console.log(gameBoard) ;
-                    break ;
-                }
-            }
-        }
-        if(gameBoard[0][0] === "O" && gameBoard[0][1] === "O" && gameBoard[0][2] === "O" ||
-            gameBoard[1][0] === "O" && gameBoard[1][1] === "O" && gameBoard[1][2] === "O" ||
-            gameBoard[2][0] === "O" && gameBoard[2][1] === "O" && gameBoard[2][2] === "O" ||
-            gameBoard[0][0] === "O" && gameBoard[1][0] === "O" && gameBoard[2][0] === "O" ||
-            gameBoard[0][1] === "O" && gameBoard[1][1] === "O" && gameBoard[2][1] === "O" ||
-            gameBoard[0][2] === "O" && gameBoard[1][2] === "O" && gameBoard[2][2] === "O" ||
-            gameBoard[0][0] === "O" && gameBoard[1][1] === "O" && gameBoard[2][2] === "O" ||
-            gameBoard[0][2] === "O" && gameBoard[1][1] === "O" && gameBoard[2][0] === "O"
-        ){
-            console.log("Computer Won") ;
-            break ;
-        }
-    }
+//         [input,row,column] = getCompInput() ;
+//         if (gameBoard[row][column] === '') {
+//             gameBoard[row][column] = "O" ;
+//             console.log(gameBoard) ;
+//         }
+//         else{
+//             while(true){
+//                 [input,row,column] = getCompInput() ;
+//                 if (gameBoard[row][column] === '') {
+//                     gameBoard[row][column] = "O" ;
+//                     console.log(gameBoard) ;
+//                     break ;
+//                 }
+//             }
+//         }
+//         if(gameBoard[0][0] === "O" && gameBoard[0][1] === "O" && gameBoard[0][2] === "O" ||
+//             gameBoard[1][0] === "O" && gameBoard[1][1] === "O" && gameBoard[1][2] === "O" ||
+//             gameBoard[2][0] === "O" && gameBoard[2][1] === "O" && gameBoard[2][2] === "O" ||
+//             gameBoard[0][0] === "O" && gameBoard[1][0] === "O" && gameBoard[2][0] === "O" ||
+//             gameBoard[0][1] === "O" && gameBoard[1][1] === "O" && gameBoard[2][1] === "O" ||
+//             gameBoard[0][2] === "O" && gameBoard[1][2] === "O" && gameBoard[2][2] === "O" ||
+//             gameBoard[0][0] === "O" && gameBoard[1][1] === "O" && gameBoard[2][2] === "O" ||
+//             gameBoard[0][2] === "O" && gameBoard[1][1] === "O" && gameBoard[2][0] === "O"
+//         ){
+//             console.log("Computer Won") ;
+//             break ;
+//         }
+//     }
+// }
+function singlePlayerFunction(){
+
 }
 
 function toShowMessage(){
@@ -187,3 +190,15 @@ function toShowMessage(){
     })
 };
 toShowMessage()
+
+function againstComp(){
+    const playingSection = document.querySelector("#playing-section")
+    const compBtn = document.querySelector(".computer")
+    const welcomeBox = document.querySelector("#welcome-box");
+    compBtn.addEventListener("click",()=>{
+        welcomeBox.style.display = 'none';
+        playingSection.style.display = 'flex' ;
+        singlePlayerFunction()
+    })
+}
+againstComp()
